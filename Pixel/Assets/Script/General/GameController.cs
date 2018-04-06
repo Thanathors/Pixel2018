5a37524d69941747897da49327a5f2cd58e826f0
zeloswaldo <jer_black10@outlook.com>
2018-04-06 02:11:57 -0400

    UI update

--- Pixel/Assets/Script/General/GameController.cs
+++ Pixel/Assets/Script/General/GameController.cs
@@ -24,6 +24,11 @@ public class GameController : MonoBehaviour {
     public static int cloth_count;
     public static int trivia_count;
     public static int key_count;
+    private Text animal_text;
+    private Text wallet_text;
+    private Text cloth_text;
+    private Text trivia_text;
+    private Text key_text;
 
     private void Awake()
     {
@@ -37,6 +42,13 @@ public class GameController : MonoBehaviour {
     // Use this for initialization
     void Start () {
         ini_timer = timer;
+
+        animal_text = GameObject.Find("Animal_Counter").GetComponent<Text>();
+        wallet_text = GameObject.Find("Wallet_Counter").GetComponent<Text>();
+        cloth_text = GameObject.Find("Cloth_Counter").GetComponent<Text>();
+        key_text = GameObject.Find("Key_Counter").GetComponent<Text>();
+        trivia_text = GameObject.Find("Trivia_Counter").GetComponent<Text>();
+
         if (GameObject.Find("Timer_Slider"))
         {
             timer_Img = GameObject.Find("Timer_Slider").GetComponent<Image>();
@@ -52,6 +64,12 @@ public class GameController : MonoBehaviour {
 	void Update () {
         timer -= Time.deltaTime;
         timer_Img.fillAmount = timer / ini_timer;
+
+        animal_text.text = "Animal:" + animal_count.ToString();
+        wallet_text.text = "Wallet:" + wallet_count.ToString();
+        cloth_text.text = "Cloth:" + cloth_count.ToString();
+        key_text.text = "Key:" + key_count.ToString();
+        trivia_text.text = "Trivia:" + trivia_count.ToString();
 
         if(timer <= 0)
         {
