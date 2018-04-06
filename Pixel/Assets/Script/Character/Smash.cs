f7984b2a83fd9e0248dc1409af6a942c09364934
David Larouche <david_larouche@msn.com>
2018-04-06 00:47:01 -0400

    Montre la main à la library

---
+++ Pixel/Assets/Script/Character/Smash.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Smash : MonoBehaviour {
+
+    Animator m_Anim;
+    [ReadOnly]
+    public bool breaking;
+
+	void Start ()
+    {
+        m_Anim = GetComponent<Animator>();
+	}
+	
+	void Update ()
+    {
+        BreakCheck();
+        if (Input.GetKeyUp(KeyCode.Mouse0) && m_Anim.GetBool("Smash") == false)
+        {
+            m_Anim.SetBool("Smash", true);
+        }
+	}
+
+    void BreakCheck()
+    {
+        if (m_Anim.GetBool("Smash") == true)
+        {
+            breaking = true;
+        }
+
+        else
+        {
+            breaking = false;
+        }
+    }
+
+    public void SmashToFalse()
+    {
+        m_Anim.SetBool("Smash", false);
+    }
+}
---
+++ Pixel/Assets/Script/GPI/ShatterManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShatterManager : MonoBehaviour {
+
+    public bool isBreakable;
+    public GameObject shatteredObject;
+
+	void Start ()
+    {
+		
+	}
+
+    void Update()
+    {
+        if (isBreakable)
+        {
+            Instantiate(shatteredObject, transform.position, transform.rotation);
+            Destroy(gameObject);
+        }
+	}
+}
