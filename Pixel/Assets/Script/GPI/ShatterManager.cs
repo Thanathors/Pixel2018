b13a9dfcebabec983332b0095b492b4b481b6b22
David Larouche <david_larouche@msn.com>
2018-04-06 19:46:35 -0400

    Hope it doesn't FUCKING EXPLODe
    Animation, Script, Ma Scene,

--- Pixel/Assets/Script/GPI/ShatterManager.cs
+++ Pixel/Assets/Script/GPI/ShatterManager.cs
@@ -19,7 +19,27 @@ public class ShatterManager : MonoBehaviour {
             Instantiate(shatteredObject, transform.position, transform.rotation);
             Destroy(gameObject);
         }
-	}
+    }
+
+    private void FixedUpdate()
+    {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.transform.gameObject.tag == "Item")
+            {
+                Debug.Log("Bitch");
+                if (Vector3.Distance(transform.position, player.transform.position) < 2f && player.GetComponent<Smash>().breaking)
+                {
+                    isBreakable = true;
+                }
+            }
+        }
+    }
 
     private void OnCollisionEnter(Collision collision)
     {
