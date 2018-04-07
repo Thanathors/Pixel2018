293626d80e1f3f3ec21a7b8ca7e266fc6cc03177
David Larouche <david_larouche@msn.com>
2018-04-07 11:03:29 -0400

    Revert "Revert "Merge remote-tracking branch 'origin/master'""
    This reverts commit 75de537015fe17b1fb5789de0bc42159802ac9cc.

--- Pixel/Assets/Script/General/ScoreScreen_Controller.cs
+++ Pixel/Assets/Script/General/ScoreScreen_Controller.cs
@@ -13,6 +13,8 @@ public class ScoreScreen_Controller : MonoBehaviour {
     private Text obj_collected_text;
     private Text obj_destroyed_text;
     private int score = 0;
+    private Slider slider;
+    private Text stormRank;
 
     private void Start()
     {
@@ -20,6 +22,7 @@ public class ScoreScreen_Controller : MonoBehaviour {
         Award_Des_txt = GameObject.Find("Award_Description").GetComponent<Text>();
         obj_collected_text = GameObject.Find("Object_Collected_text").GetComponent<Text>();
         obj_destroyed_text = GameObject.Find("Object_Destroyed_text").GetComponent<Text>();
+        stormRank = GameObject.Find("Rank_text").GetComponent<Text>();
     }
 
     // Update is called once per frame
@@ -31,13 +34,13 @@ public class ScoreScreen_Controller : MonoBehaviour {
 
         if (GameController.total_count == 1)
         {
-            award = "Gotta go fast!";
-            Award_Des_txt.text = "Leave the house without picking anything.";
+            award = "To The Point";
+            Award_Des_txt.text = "Picked up Wallet and left.";
         }
         if(GameController.total_count == -1)
         {
-            award = "Nobody care anymore...";
-            Award_Des_txt.text = "Take too much time to storm-out.";
+            award = "Lost Credibility";
+            Award_Des_txt.text = "Took too much time.";
         }
         //script needed
         if (GameController.total_count == 10000)
@@ -94,29 +97,35 @@ public class ScoreScreen_Controller : MonoBehaviour {
 
         if(score < 3)
         {
+            stormRank.text = "Baby Storm"; 
             //Baby Storm
         }
         else if(score >=3 && score < 5)
         {
+            stormRank.text = "Storm Out";
             //Storm Out
         }
         else if (score >= 5 && score < 7)
         {
+            stormRank.text = "Ok Storm";
             //Ok Storm
         }
         else if (score >= 7 && score < 9)
         {
+            stormRank.text = "Good Storm";
             //Good Storm
         }
         else if(score ==9)
         {
+            stormRank.text = "Epic Storm";
             //Epic Storm
         }else if(score == 10)
         {
+            stormRank.text = "Perfect Storm";
             //Perfect Storm
         }
 
-        if (timer > 5)
+        if (timer > 10)
         {
             SceneManager.LoadScene(0);
         }
